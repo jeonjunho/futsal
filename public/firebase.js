bcdcdf2dd3daf444ebdd85fa06f9f1dd4120c050
JeonJunho <junho008junho@gmail.com>
2021-11-06 17:58:01 +0900

    first commit

---
+++ public/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCgFi0-DsPspIsr17-lj53uZ2J97-UWrHY",
+    authDomain: "futsal-2e70c.firebaseapp.com",
+    projectId: "futsal-2e70c",
+    storageBucket: "futsal-2e70c.appspot.com",
+    messagingSenderId: "410854573684",
+    appId: "1:410854573684:web:919b2e84ac8746f37d7321"
+};
+firebase.initializeApp(firebaseConfig);
